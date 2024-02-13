044681075e9f2154d1f5cf3f05895284b9d0d0e9
kimchinichi <152254653+kimchinichi@users.noreply.github.com>
2024-02-13 07:17:28 -0700

    Update script (1).js

--- script (1).js
+++ script (1).js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yay, see you on the 18th!";
+  question.innerHTML = "Yay, see you on the 14th!";
   gif.src =
     "https://media.giphy.com/media/lkZOrpBvYEePeqZVAn/giphy.gif";
 });
