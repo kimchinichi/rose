853cd23537687119b50aa0ee18d1fed0e9abecee
kimchinichi <152254653+kimchinichi@users.noreply.github.com>
2024-02-13 07:15:53 -0700

    Update script (1).js

--- script (1).js
+++ script (1).js
@@ -7,7 +7,7 @@ const noBtn = document.querySelector(".no-btn");
 yesBtn.addEventListener("click", () => {
   question.innerHTML = "Yay, see you on the 18th!";
   gif.src =
-    "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
+    "https://media.giphy.com/media/lkZOrpBvYEePeqZVAn/giphy.gif";
 });
 
 noBtn.addEventListener("mouseover", () => {
